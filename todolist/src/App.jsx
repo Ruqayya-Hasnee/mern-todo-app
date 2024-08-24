07563c712a89291a891387e026ed0db8960033b1
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2024-08-24 20:51:03 +0500

    first commit

---
+++ todolist/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './home'
+
+function App() {
+  
+  return (
+    <div className='home'>
+        <Home />  
+    </div>
+  )
+}
+
+export default App
